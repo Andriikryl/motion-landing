771747ef06b18c89bbf2bc64b9ddcfb4457462d9
Andriikryl <andreyk557@gmail.com>
2023-06-06 19:39:34 +0300

    add Hero component to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Hero } from "./components/Hero";
+
 function App() {
-  return <div className="mx-auto max-w-6xl px-4"></div>;
+  return (
+    <div className="mx-auto max-w-6xl px-4">
+      <Hero />
+    </div>
+  );
 }
 
 export default App;
