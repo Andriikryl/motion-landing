798082715187251a0c9ec98196832ebe03968254
Andriikryl <andreyk557@gmail.com>
2023-06-06 19:48:08 +0300

    add featurs data into app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,54 @@
 import { Hero } from "./components/Hero";
 
+const features = [
+  {
+    title: "Use your calendar as a todo list",
+    id: "todo-list",
+    // card: Todo,
+    // visual: OtherVisual,
+  },
+  {
+    title: "Color your calendar to organize",
+    id: "colors",
+    // card: Colors,
+    // visual: OtherVisual,
+  },
+  {
+    title: "Instantly know if someone is available",
+    id: "availability",
+    // card: Availability,
+    // visual: OtherVisual,
+  },
+  {
+    title: "Track what you listened to when",
+    id: "music",
+    // card: Music,
+    // visual: MusicVisual,
+  },
+  {
+    title: "Send scheduling links guests love",
+    id: "scheduling-links",
+    // card: SchedulingLinks,
+    // visual: OtherVisual,
+  },
+  {
+    title: "Always know what your team is up to",
+    id: "team",
+    // card: Team,
+    // visual: OtherVisual,
+  },
+];
+
 function App() {
   return (
     <div className="mx-auto max-w-6xl px-4">
       <Hero />
+      <div className="flex w-full gap-20">
+        <div className="w-full">
+          <ul></ul>
+        </div>
+        <div className="w-full">right col</div>
+      </div>
     </div>
   );
 }
